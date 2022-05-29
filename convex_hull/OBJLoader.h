0b8a0d965d14f2c237329d75c2c2634f6acbfa55
Gabriel Freire <gabrielfvale@gmail.com>
2022-05-29 14:41:33 -0300

    Add OBJ loader

---
+++ convex_hull/OBJLoader.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
+
+#include <SDL2/SDL.h>
+
+static std::tuple<SDL_FPoint *, int> loadOBJ(const char *file_name, int w, int h)
+{
+
+  std::stringstream ss;
+  std::ifstream in_file(file_name);
+  std::string line = "";
+  std::string prefix = "";
+  SDL_FPoint temp;
+  std::vector<SDL_FPoint> v;
+  int half_w = w / 2;
+  int half_h = h / 2;
+
+  if (!in_file.is_open())
+  {
+    throw "Obj loader failed";
+  }
+
+  while (std::getline(in_file, line))
+  {
+    ss.clear();
+    ss.str(line);
+    ss >> prefix;
+
+    std::string coordx = "";
+    std::string coordy = "";
+
+    if (prefix != "v")
+    {
+      continue;
+    }
+
+    ss >> coordx >> coordy;
+    temp.x = std::stof(coordx) + half_w;
+    temp.y = std::stof(coordy) + half_h;
+
+    v.push_back(temp);
+  }
+
+  SDL_FPoint *result = new SDL_FPoint[v.size()];
+  memcpy(result, &v.front(), v.size() * sizeof(SDL_FPoint));
+
+  return std::make_tuple(result, v.size());
+}
--- convex_hull/main.cpp
+++ convex_hull/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <tuple>
+
+#include "OBJLoader.h"
 
 int main(int args, char *argv[])
 {
+
+  if (args < 2)
+  {
+    std::cerr << "main() Error: OBJ filename not provided" << std::endl;
+    return 1;
+  }
+
+  const char *filename = argv[1];
+  const std::string title = "Convex Hull: " + std::string(filename);
 
   const int windowWidth = 640;
   const int windowHeight = 480;
@@ -22,7 +34,7 @@ int main(int args, char *argv[])
 
   // Create window
   window = SDL_CreateWindow(
-      "Convex Hull",
+      title.c_str(),
       SDL_WINDOWPOS_CENTERED,
       SDL_WINDOWPOS_CENTERED,
       windowWidth, windowHeight,
@@ -46,6 +58,10 @@ int main(int args, char *argv[])
   const int pointLocationx = windowWidth / 2;
   const int pointLocationy = windowHeight / 2;
 
+  SDL_FPoint *points;
+  int size;
+  std::tie(points, size) = loadOBJ(filename, windowWidth, windowHeight);
+
   SDL_Event event;
   bool quit = false;
 
@@ -65,7 +81,14 @@ int main(int args, char *argv[])
     // Set color of renderer
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_RenderDrawPoint(renderer, pointLocationx, pointLocationy);
+    SDL_RenderDrawPointsF(renderer, points, size);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (int i = 0; i < size - 1; ++i)
+    {
+      SDL_RenderDrawLine(renderer, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+    }
+    SDL_RenderDrawLine(renderer, points[size - 1].x, points[size - 1].y, points[0].x, points[0].y);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
